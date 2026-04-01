a31b9dae9eaf31f6f52d35112e2dd0527bca72f1
Rohit Dagar <dagarr685@gmail.com>
2026-04-01 15:51:30 +0530

    fix unused import warning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Chart as ChartJS } from 'chart.js/auto';
+import 'chart.js/auto';
 import { Bar } from 'react-chartjs-2';
 import './App.css';
 
